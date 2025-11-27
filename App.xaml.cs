4693c2809b8037c4a584e23db5a46720844f5850
timur <timurkkq@gmail.com>
2025-11-27 20:02:18 +0300

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Deushev_Gvozdeva_ISP_231;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
